d2b805015b4e24e5144cf476aed6e8c5f196cd91
dearnono2 <dearnono2@gmail.com>
2022-11-01 16:15:58 +0900

    react slider loop

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Btns from "./Btns";
+import GlobalStyle from "./GlobalStyle";
+import Panels from "./Panels";
+import { useRef } from 'react';
+
+function App() {
+	const panel = useRef(null);
+
+	return (
+		<>
+			<GlobalStyle />
+			<Btns panel={panel} />
+			<Panels ref={panel} />
+		</>
+	);
+}
+
+export default App;
